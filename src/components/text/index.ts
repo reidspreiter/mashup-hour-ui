c993482d4fb4de29632a84a8eacf978a46ea5cf0
Reid Spreiter <reidspreiter@gmail.com>
2025-07-01 19:30:02 -0500

    src/components/text: move common text components and add Label component

--- src/components/text/index.ts
+++ src/components/text/index.ts
@@ -1,6 +1,6 @@
-export * from "./Header";
-export * from "./SubHeader";
+export * from "./Text";
 export { default as ScrollingText } from "./ScrollingText";
 export { default as Tooltip } from "./Tooltip";
 export { default as CountdownTimer } from "./CountdownTimer";
 export * from "./Icon";
+export { default as Label } from "./Label";

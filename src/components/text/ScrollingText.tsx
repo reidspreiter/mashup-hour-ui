060dfa5f6271e44ff39c273256556d843c242e3b
Reid Spreiter <reidspreiter@gmail.com>
2025-07-12 23:48:21 -0500

    src/components: fix ScrollingText bugs
    Fix bug where text got cut off on different screen sizes before
    reaching the total width of its parent.
    
    Fix bug where text took longer to dissapear than other elements when
    hiding mashup controls.

--- src/components/text/ScrollingText.tsx
+++ src/components/text/ScrollingText.tsx
@@ -8,19 +8,24 @@ interface Props {
 const ScrollContainer = styled.div`
   margin: 0px;
   overflow: hidden;
-  max-width: 28vw;
   white-space: nowrap;
 `;
 
 const ScrollingText: React.FC<Props> = ({ children }) => {
   const [translateTime, setTranslateTime] = useState(0);
   const [translateVal, setTranslateVal] = useState(0);
+  const [transitioning, setTransitioning] = useState(false);
   const containerRef = useRef<HTMLDivElement | null>(null);
+  const timeoutRef = useRef<number | null>(null);
 
   const transitionTimePerPixel = 0.01;
 
   const scrollForward = () => {
+    if (timeoutRef.current !== null) {
+      clearTimeout(timeoutRef.current);
+    }
     if (containerRef.current) {
+      setTransitioning(true);
       const boxWidth = containerRef.current.clientWidth;
       const textWidth = containerRef.current.children[0].scrollWidth;
       const translateVal = Math.min(boxWidth - textWidth, 0);
@@ -32,6 +37,12 @@ const ScrollingText: React.FC<Props> = ({ children }) => {
   const scrollBackward = () => {
     setTranslateTime(0.6);
     setTranslateVal(0);
+    if (timeoutRef.current !== null) {
+      clearTimeout(timeoutRef.current);
+    }
+    timeoutRef.current = window.setTimeout(() => {
+      setTransitioning(false);
+    }, 700);
   };
 
   return (
@@ -40,16 +51,15 @@ const ScrollingText: React.FC<Props> = ({ children }) => {
       onMouseEnter={() => scrollForward()}
       onMouseLeave={() => scrollBackward()}
     >
-      <span
+      <div
         style={{
           display: "inline-block",
-          transitionTimingFunction: "ease-in-out",
-          transitionDuration: `${translateTime}s`,
+          transition: transitioning ? `transform ${translateTime}s ease-in-out` : "none",
           transform: `translateX(${translateVal}px)`,
         }}
       >
         {children}
-      </span>
+      </div>
     </ScrollContainer>
   );
 };
